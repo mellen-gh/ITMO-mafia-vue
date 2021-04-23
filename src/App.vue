04055f3c08da8f0d4dbd9cd9947fb0304f5d16ff
Vlad_Work <v.volosin@itmo.ru>
2021-04-23 18:05:26 +0300

    MainWindow Rework

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,31 @@
   <div class="container card">
     <div class="center">
       <div class="logos"></div>
-      <button class="btn primary">Начать игру</button>
+      <button class="button-start"><span>Начать игру</span></button>
     </div>
+
     <div class="menu-lab">
       <div class="center">
         <h3>Текущие игры</h3>
       </div>
-      <div class="scrollable">
-
+      <div class="scrollableActive">
+        <ul class="ulBorder">
+          <li>
+            <div class="gamePlayers">Артём П,Влад В,Артём П,Артём П,Влад В,Артём П</div>
+          </li>
+          <li>
+            <div class="gamePlayers">Артём П,Влад В,Артём П,Артём П,Влад В,Артём П</div>
+          </li>
+          <li>
+            <div class="gamePlayers">Артём П,Влад В,Артём П,Артём П,Влад В,Артём П</div>
+          </li>
+          <li>
+            <div class="gamePlayers">Артём П,Влад В,Артём П,Артём П,Влад В,Артём П</div>
+          </li>
+        </ul>
       </div>
       <div class="center">
+
         <h3>Прошедшие игры</h3>
       </div>
       <div class="scrollable">
@@ -29,8 +44,22 @@
           <li>
             <div class="gamePlayers">Артём П,Влад В,Артём П,Артём П,Влад В,Артём П</div>
           </li>
+          <li>
+            <div class="gamePlayers">Артём П,Влад В,Артём П,Артём П,Влад В,Артём П</div>
+          </li>
+          <li>
+            <div class="gamePlayers">Артём П,Влад В,Артём П,Артём П,Влад В,Артём П</div>
+          </li>
+          <li>
+            <div class="gamePlayers">Артём П,Влад В,Артём П,Артём П,Влад В,Артём П</div>
+          </li>
         </ul>
       </div>
+    </div>
+    <div class="footer">
+      <span class="copyright">
+          © 2021 vMafia
+      </span>
     </div>
   </div>
 
